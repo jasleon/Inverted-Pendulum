a1e682ee01afadc1714c13afd2164ff6f0078dd6
Antonio Sanchez <antonio.buap@gmail.com>
2021-03-10 07:44:10 -0600

    feat: Add parameters of the system

--- inverted_pendulum.cpp
+++ inverted_pendulum.cpp
@@ -2,8 +2,17 @@
 
 #include "Eigen/Dense"
 
-InvertedPendulum::InvertedPendulum() {
-  x_ = Eigen::VectorXd(4);
+InvertedPendulum::InvertedPendulum()
+    : M_(1.0),
+      m_(1.0),
+      J_(1.0),
+      l_(1.0),
+      c_(1.0),
+      gamma_(1.0),
+      g_(9.81),
+      M_t_(2.0),
+      J_t_(2.0),
+      x_(Eigen::VectorXd(4)) {
   x_ << 0, 0, 0, 0;
 }
 
--- inverted_pendulum.h
+++ inverted_pendulum.h
@@ -5,6 +5,16 @@
 
 class InvertedPendulum {
  private:
+  const double M_;      // mass of the base
+  const double m_;      // mass of the pendulum
+  const double J_;      // moment of inertia of the pendulum
+  const double l_;      // distance from the base to the pendulum
+  const double c_;      // coefficient of viscous friction (base)
+  const double gamma_;  // coefficient of viscous friction (pendulum)
+  const double g_;      // acceleration due to gravity
+  const double M_t_;    // total mass
+  const double J_t_;    // total inertia
+  
   Eigen::VectorXd x_;
 
  public:
