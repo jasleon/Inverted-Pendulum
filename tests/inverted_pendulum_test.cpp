07ffd8060a24420de94ca81241bef971b723ba20
Antonio Sanchez <antonio.buap@gmail.com>
2021-03-13 19:38:45 -0600

    test: Improve default constructor test

--- tests/inverted_pendulum_test.cpp
+++ tests/inverted_pendulum_test.cpp
@@ -2,9 +2,19 @@
 
 #include <gtest/gtest.h>
 
-TEST(InvertedPendulumTest, Constructor) {
+TEST(InvertedPendulumTest, DefaultConstructor) {
   InvertedPendulum model;
   Eigen::VectorXd x(4);
   x << 0, 0, 0, 0;
-  EXPECT_EQ(x, model.GetState());
+
+  ASSERT_EQ(x, model.GetState());
+  ASSERT_EQ(1, model.M_);
+  ASSERT_EQ(1, model.m_);
+  ASSERT_EQ(1, model.J_);
+  ASSERT_EQ(1, model.l_);
+  ASSERT_EQ(1, model.c_);
+  ASSERT_EQ(1, model.gamma_);
+  ASSERT_EQ(9.81, model.g_);
+  ASSERT_EQ(2, model.M_t_);
+  ASSERT_EQ(2, model.J_t_);
 }
