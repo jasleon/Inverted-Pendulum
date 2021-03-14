0475c6f9eb42c0e65e2ebcc41ad3e87f01b09fd0
Antonio Sanchez <antonio.buap@gmail.com>
2021-03-13 20:20:06 -0600

    feat: Simulate inverted pendulum

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
+#include <sciplot/sciplot.hpp>
 
+#include "Eigen/Dense"
 #include "inverted_pendulum.h"
 
 int main() {
-  InvertedPendulum model;
-  double t = 0;
-  while(t < 1.0)
-  {
-    t += 0.001;
+  // Set initial conditions
+  Eigen::VectorXd x_0(4);
+  x_0 << 0, M_PI / 4, 0, 0;
+
+  // Create a model with default parameters
+  InvertedPendulum model(x_0);
+
+  // Define simulation time
+  sciplot::Vec time = sciplot::linspace(0.0, 20.0, 1000);
+
+  // Save the state of the model
+  std::vector<double> theta;
+
+  // Simulate model
+  for (auto t : time) {
     model.Update(t, 0);
+
+    Eigen::VectorXd x = model.GetState();
+    theta.push_back(x(1));
   }
-  std::cout << model.GetState() << std::endl;
+
+  // Create Plot object
+  sciplot::Plot plot;
+
+  // Set color palette
+  plot.palette("set2");
+
+  // Draw theta over the simulation time
+  plot.drawCurve(time, theta).label("theta(t)").lineWidth(4);
+
+  // Show the plot in a pop-up window
+  plot.show();
 }
