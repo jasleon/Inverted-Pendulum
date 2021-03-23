d15b081a038a43b639159478fcf79801e143a43b
Antonio Sanchez <antonio.buap@gmail.com>
2021-03-22 20:30:43 -0600

    feat: Add rigid rod to the visualization

--- scripts/visualizer.py
+++ scripts/visualizer.py
@@ -9,7 +9,7 @@ def generate_mass1():
     return x, y
 
 def generate_mass2(x_mass1, y_mass1):
-    theta = 0.25 * np.pi
+    theta = np.arange(0, np.pi, np.pi / x_mass1.size)
     phi = 0.5 * np.pi
     x = x_mass1 + 0.5 * np.cos(theta + phi)
     y = y_mass1 + 0.5 * np.sin(theta + phi)
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     for i, arg in enumerate(sys.argv):
         print('argv[{}]: {}'.format(i, arg))
     fig = plt.figure(figsize=(5, 4))
-    ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 1))
+    ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-1, 1))
     ax.set_aspect('equal')
     ax.grid()
 
@@ -32,17 +32,22 @@ if __name__ == "__main__":
     mass2, = ax.plot([],[],linestyle='None',marker='o',\
                  markersize=20,markeredgecolor='k',\
                  color='orange',markeredgewidth=2)
+    line, = ax.plot([],[],'o-',color='black',lw=4,\
+                markersize=6,markeredgecolor='k',\
+                markerfacecolor='k')
     
     def init():
         mass1.set_data([], [])
         mass2.set_data([], [])
-        return mass1, mass2,
+        line.set_data([], [])
+        return mass1, mass2, line
     
     def animate(i):
         mass1.set_data([m1_x[i]], [m1_y[i]])
         mass2.set_data([m2_x[i]], [m2_y[i]])
-        return mass1, mass2,
+        line.set_data([m1_x[i], m2_x[i]], [m1_y[i], m2_y[i]])
+        return mass1, mass2, line
     
-    ani = animation.FuncAnimation(fig, animate, m1_x.size, interval=10, repeat=False, blit=True)
+    ani = animation.FuncAnimation(fig, animate, m1_x.size, interval=10, repeat=True, blit=True)
     
     plt.show()
