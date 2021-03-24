199c61bddd9490c3597702c4b455d77da6e54c17
Antonio Sanchez <antonio.buap@gmail.com>
2021-03-23 21:51:08 -0600

    feat: Create a function to write to files

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,1 +1,16 @@
 #include "tools.h"
+
+#include <fstream>
+
+void foo(const std::vector<double>& values) {
+  std::ofstream output;
+
+  output.open("test.txt");
+
+  if (output.is_open()) {
+    for (const auto& v : values) {
+      output << v << std::endl;
+    }
+    output.close();
+  }
+}
--- src/tools.h
+++ src/tools.h
@@ -1,4 +1,8 @@
 #ifndef TOOLS_H_
 #define TOOLS_H_
 
+#include <vector>
+
+void foo(const std::vector<double> &values);
+
 #endif  // TOOLS_H_
