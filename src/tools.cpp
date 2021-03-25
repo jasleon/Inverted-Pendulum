2e0af7155ef579c85ba9b11091f2eb87053e4212
Antonio Sanchez <antonio.buap@gmail.com>
2021-03-24 20:30:45 -0600

    feat: Save simulation data to CSV

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,3 +14,18 @@ void foo(const std::vector<double>& values) {
     output.close();
   }
 }
+
+void Save(std::string filename, const std::vector<double>& time,
+          std::vector<double>& position, const std::vector<double>& angle) {
+  if (position.size() == angle.size()) {
+    std::ofstream output;
+    output.open(filename);
+    if (output.is_open()) {
+      output << "time,position,angle\n";
+      for (size_t i = 0; i < position.size(); ++i) {
+        output << time[i] << "," << position[i] << "," << angle[i] << "\n";
+      }
+      output.close();
+    }
+  }
+}
--- src/tools.h
+++ src/tools.h
@@ -1,8 +1,12 @@
 #ifndef TOOLS_H_
 #define TOOLS_H_
 
+#include <string>
 #include <vector>
 
 void foo(const std::vector<double> &values);
+
+void Save(std::string filename, const std::vector<double> &time,
+          std::vector<double> &position, const std::vector<double> &angle);
 
 #endif  // TOOLS_H_
