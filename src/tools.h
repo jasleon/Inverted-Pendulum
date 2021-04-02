384d8ae80865537ce0e3e8dd739a0ce53d703d6d
Antonio Sanchez <antonio.buap@gmail.com>
2021-04-01 19:31:41 -0600

    feat: Add short description to function

--- src/tools.h
+++ src/tools.h
@@ -4,6 +4,12 @@
 #include <string>
 #include <vector>
 
+/**
+ * Exports simulation data to a comma-delimited file
+ * @param filename File name
+ * @param columns Name of the columns in the data matrix
+ * @param data 2D matrix with values from the simulation
+ */ 
 void Export(std::string filename, std::vector<std::string> columns,
             std::vector<std::vector<double>> &data);
 
