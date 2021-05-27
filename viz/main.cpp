5691cee9a6a8ddee8831071bc5a1fd2c569d02a1
Antonio Sanchez <antonio.buap@gmail.com>
2021-05-27 08:05:26 -0500

    feat: Connect base with physics

--- viz/main.cpp
+++ viz/main.cpp
@@ -1,6 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+#include "Eigen/Dense"
+#include "inverted_pendulum.h"
+
 int main() {
   sf::RenderWindow window(sf::VideoMode(640, 480), "Inverted Pendulum");
 
@@ -8,8 +11,13 @@ int main() {
   base.setOrigin(50.0F, 50.0F);
   base.setPosition(320.0F, 240.0F);
   base.setFillColor(sf::Color::Green);
+  base.setOutlineColor(sf::Color::Black);
+  base.setOutlineThickness(2.5);
 
   sf::Clock clock;
+  Eigen::VectorXd x_0(4);
+  x_0 << 0, M_PI / 4, 0, 0;
+  InvertedPendulum model(x_0);
 
   while (window.isOpen()) {
     sf::Event event;
@@ -22,7 +30,11 @@ int main() {
     }
     sf::Time elapsed = clock.getElapsedTime();
     std::cout << elapsed.asSeconds() << '\n';
-    window.clear();
+    model.Update(elapsed.asSeconds(), 0);
+    Eigen::VectorXd x = model.GetState();
+    std::cout << "x: " << x(0) << '\n';
+    base.setPosition(320.0F + 100 * x(0), 240.0F);
+    window.clear(sf::Color::White);
     window.draw(base);
     window.display();
   }
