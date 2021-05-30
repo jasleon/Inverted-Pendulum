8056b891df7639d878f9e453850a63e1a238ca71
Antonio Sanchez <antonio.buap@gmail.com>
2021-05-29 19:07:54 -0500

    feat: Draw pole physics

--- viz/main.cpp
+++ viz/main.cpp
@@ -4,19 +4,30 @@
 #include "Eigen/Dense"
 #include "inverted_pendulum.h"
 
+double to_radians(double degrees) { return (degrees * (M_PI / 180.0F)); }
+
+double to_degrees(double radians) { return (radians * (180.0F / M_PI)); }
+
 int main() {
   sf::RenderWindow window(sf::VideoMode(640, 480), "Inverted Pendulum");
 
+  // Initial conditions
+  const double theta_0 = -30.0F;
+  
   sf::RectangleShape base(sf::Vector2f(100.0F, 100.0F));
   base.setOrigin(50.0F, 50.0F);
   base.setPosition(320.0F, 240.0F);
-  base.setFillColor(sf::Color::Green);
-  base.setOutlineColor(sf::Color::Black);
-  base.setOutlineThickness(2.5);
+  base.setFillColor(sf::Color::Blue);
+
+  sf::RectangleShape pole(sf::Vector2f(20.0F, 200.0F));
+  pole.setOrigin(10.0F, 200.0F);
+  pole.setPosition(320.0F, 240.0F);
+  pole.setRotation(-theta_0);
+  pole.setFillColor(sf::Color::Green);
 
   sf::Clock clock;
   Eigen::VectorXd x_0(4);
-  x_0 << 0, M_PI / 4, 0, 0;
+  x_0 << 0, to_radians(theta_0), 0, 0;
   InvertedPendulum model(x_0);
 
   while (window.isOpen()) {
@@ -34,8 +45,11 @@ int main() {
     Eigen::VectorXd x = model.GetState();
     std::cout << "x: " << x(0) << '\n';
     base.setPosition(320.0F + 100 * x(0), 240.0F);
+    pole.setPosition(320.0F + 100 * x(0), 240.0F);
+    pole.setRotation(to_degrees(-x(1)));
     window.clear(sf::Color::White);
     window.draw(base);
+    window.draw(pole);
     window.display();
   }
   return 0;
