192066a53f0b08d05f0d55d4fa8350a5557e88fb
Antonio Sanchez <antonio.buap@gmail.com>
2021-06-22 10:25:05 -0500

    feat: Compute LQR gain K

--- src/lqr.h
+++ src/lqr.h
@@ -9,7 +9,11 @@ class LQR {
   Eigen::MatrixXd B_;
   Eigen::MatrixXd Q_;
   Eigen::MatrixXd R_;
+  Eigen::MatrixXd P_;
   Eigen::MatrixXd K_;
+
+  void Compute();
+  Eigen::VectorXd Control(const Eigen::VectorXd &x);
 };
 
 #endif  // LQR_H_
