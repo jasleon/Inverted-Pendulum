273601375a4ab0d8c33ea2149add356899535edb
Antonio Sanchez <antonio.buap@gmail.com>
2021-06-24 10:37:56 -0500

    test: Add test for the linearization method

--- tests/inverted_pendulum_test.cpp
+++ tests/inverted_pendulum_test.cpp
@@ -18,3 +18,33 @@ TEST(InvertedPendulumTest, DefaultConstructor) {
   ASSERT_EQ(2, model.M_t_);
   ASSERT_EQ(2, model.J_t_);
 }
+
+TEST(InvertedPendulumTest, Linearize) {
+  InvertedPendulum model;
+  model.Linearize();
+
+  Eigen::MatrixXd exp_A = Eigen::MatrixXd::Zero(4, 4);
+  exp_A << 0, 0, 1, 0, 
+           0, 0, 0, 1, 
+           0, 3.27, -0.6667, -0.3333, 
+           0, 6.54, -0.3333, -0.6667;
+  
+  Eigen::MatrixXd exp_B = Eigen::MatrixXd::Zero(4, 1);
+  exp_B << 0, 0, 0.6667, 0.3333;
+
+  size_t n = exp_A.rows();
+  size_t m = exp_A.cols();
+  for (size_t i = 0; i < n; ++i) {
+    for (size_t j = 0; j < m; ++j) {
+      EXPECT_NEAR(exp_A(i, j), model.A_(i, j), 1e-3);
+    }
+  }
+
+  n = exp_B.rows();
+  m = exp_B.cols();
+  for (size_t i = 0; i < n; ++i) {
+    for (size_t j = 0; j < m; ++j) {
+      EXPECT_NEAR(exp_B(i, j), model.B_(i, j), 1e-3);
+    }
+  }
+}
